0b06fa10e118ca6c012088598008abaeb28809ae
James Wright <hi@james.engineering>
2023-04-16 17:21:59 +0100

    Adds player rendering and movement

--- src/raycaster/index.ts
+++ src/raycaster/index.ts
@@ -1,3 +1,19 @@
+const createKeys = () => {
+  const keys = new Set<string>;
+
+  window.addEventListener('keydown', ({ key }) => {
+    keys.add(key);
+  });
+
+  window.addEventListener('keyup', ({ key }) => {
+    keys.delete(key);
+  });
+
+  const isPressed = (key: string) => keys.has(key);
+
+  return isPressed;
+};
+
 const context = document
   .querySelector<HTMLCanvasElement>("#canvas")
   ?.getContext("2d");
@@ -8,15 +24,55 @@ if (!context) {
   );
 }
 
+interface Entity {
+  update(): void
+}
+
+const PLAYER_SIZE = 32;
+
+const isKeyPressed = createKeys();
+
+const createPlayer = (x: number, y: number, speed: number) => ({
+  x,
+  y,
+  speed,
+  update() {
+    if (isKeyPressed('w')) {
+      this.y -= speed;
+    }
+
+    if (isKeyPressed('s')) {
+      this.y += speed;
+    }
+
+    if (isKeyPressed('a')) {
+      this.x -= speed;
+    }
+
+    if (isKeyPressed('d')) {
+      this.x += speed;
+    }
+
+    context.fillStyle = 'green';
+    context.fillRect(this.x, this.y, PLAYER_SIZE, PLAYER_SIZE);
+  },
+});
+
 const FPS = 60;
 const TICK_INTERVAL_MS = 1000 / FPS;
 
 let lastTick = 0;
 
+const entities: Entity[] = [createPlayer(300, 300, 5)];
+
 const loop = (tick: DOMHighResTimeStamp) => {
   if (tick - lastTick >= TICK_INTERVAL_MS) {
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    context.fillText(tick + "", 20, 20);
+
+    for (const entity of entities) {
+      entity.update();
+    }
+
     lastTick = tick;
   }
 
