cfc04f118f5175939b38ce525b413073998d3571
James Wright <hi@james.engineering>
2023-04-16 17:31:41 +0100

    Renders map

--- src/raycaster/index.ts
+++ src/raycaster/index.ts
@@ -28,6 +28,30 @@ interface Entity {
   update(): void
 }
 
+const GRID_ITEM_SIZE = 64;
+
+const map = [
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 1, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+const createMapRenderer = () => ({
+  update() {
+    for (let row = 0; row < map.length; row++) {
+      for (let col = 0; col < map[row].length; col++) {
+        context.fillStyle = map[row][col] === 1 ? 'white' : 'black';
+        context.fillRect(GRID_ITEM_SIZE * col, GRID_ITEM_SIZE * row, GRID_ITEM_SIZE, GRID_ITEM_SIZE);
+      }
+    }
+  }
+});
+
 const PLAYER_SIZE = 32;
 
 const isKeyPressed = createKeys();
@@ -63,7 +87,7 @@ const TICK_INTERVAL_MS = 1000 / FPS;
 
 let lastTick = 0;
 
-const entities: Entity[] = [createPlayer(300, 300, 5)];
+const entities: Entity[] = [createMapRenderer(), createPlayer(300, 300, 5)];
 
 const loop = (tick: DOMHighResTimeStamp) => {
   if (tick - lastTick >= TICK_INTERVAL_MS) {
