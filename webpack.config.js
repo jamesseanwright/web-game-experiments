1db57b4534f82e5b6df4ba658257f92196728a2b
James Wright <hi@james.engineering>
2023-04-16 16:18:54 +0100

    Bootstraps build

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = ({ project }) => ({
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: `./src/${project}/index.ts`,
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+});
